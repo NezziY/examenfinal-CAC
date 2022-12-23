2ae37ea316a4a1b5fead6d93f585469dd5c5158d
NezziY <113919604+NezziY@users.noreply.github.com>
2022-12-22 22:18:34 -0300

    Add files via upload

---
+++ build/web/main.js
@@ -0,0 +1,60 @@
+
+let item1 = document.getElementById("item-1");
+item1.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "Bolsita <br>";
+});
+
+let item2 = document.getElementById("item-2");
+item2.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "Taza <br>";
+});
+
+let item3 = document.getElementById("item-3");
+item3.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "Tuxedo <br>";
+});
+
+let item4 = document.getElementById("item-4");
+item4.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "Oliva <br>";
+});
+
+let item5 = document.getElementById("item-5");
+item5.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "Pantuflas <br>";
+});
+
+let item6 = document.getElementById("item-6");
+item6.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "Cuadro <br>";
+});
+
+let item7 = document.getElementById("item-7");
+item7.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "Remera <br>";
+});
+
+let item8 = document.getElementById("item-8");
+item8.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "Maceta <br>";
+});
+
+let item9 = document.getElementById("item-9");
+item9.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "R. Navidad <br>";
+});
+
+let item10 = document.getElementById("item-10");
+item10.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "Cristal <br>";
+});
+
+let item11 = document.getElementById("item-11");
+item11.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "Violeta <br>";
+});
+
+let item12 = document.getElementById("item-12");
+item12.addEventListener("click", ()=>{
+    document.getElementById("lista").innerHTML += "Sticker <br>";
+});
